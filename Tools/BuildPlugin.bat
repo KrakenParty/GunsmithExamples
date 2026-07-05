a7dbc1adfaf83428ed0015b336a518c1af94a682
Kieran Newland <kieran.newland@gmail.com>
2026-07-05 20:05:25 +0100

    Updated BuildPlugin.bat plugin dir

--- Tools/BuildPlugin.bat
+++ Tools/BuildPlugin.bat
@@ -3,5 +3,5 @@ set BuildPath=%2
 
 if [%BuildPath%] == [] set BuildPath=%CD%
 
-%EnginePath%/Engine/Build/BatchFiles/RunUAT.bat BuildPlugin -Plugin=%CD%/GunsmithExamples/Plugins/Gunsmith/Gunsmith.uplugin -Package=%BuildPath%/Gunsmith -TargetPlatforms=Win64+Android+Linux -Rocket -Verbose
+%EnginePath%/Engine/Build/BatchFiles/RunUAT.bat BuildPlugin -Plugin=%CD%/Plugins/Gunsmith/Gunsmith.uplugin -Package=%BuildPath%/Gunsmith -TargetPlatforms=Win64+Android+Linux -Rocket -Verbose
 PAUSE
